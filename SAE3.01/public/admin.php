3f5c50d4b0d53900779859705d071a12cbefe738
Ali-ShanKM <alishan.km15@gmail.com>
2025-01-07 01:45:07 +0100

    à quoi ça sert de dormir finalement ?

--- SAE3.01/public/admin.php
+++ SAE3.01/public/admin.php
@@ -1,1 +1,12 @@
 <?php
+if(!session_id())
+    session_start();
+
+$title = "Admin";
+require_once './header.php';
+?>
+    <main>
+        <h1>Espace administrateur</h1>
+    </main>
+<?php
+require_once './footer.php';
